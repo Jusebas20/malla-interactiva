04deb40b8c47ae5b63abeaa750ed56d713c4de08
Jusebas20 <jslaverde1@gmail.com>
2025-07-31 09:08:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Oculta el loader cuando la página termina de cargar
+window.addEventListener('load', () => {
+  const loader = document.getElementById('loader');
+  loader.style.opacity = 0;
+  setTimeout(() => loader.style.display = 'none', 500);
+});
+
 const pensum = [
   {
     semestre: 1,
